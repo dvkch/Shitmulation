6cff3e912ab9e32c811298e88e662e462a7771fb
Stanislas Chevallier <contact@stanislaschevallier.fr>
2023-05-30 13:21:28 +0200

    Make it work for 10b

--- Shitmulation/Models/Counter.swift
+++ Shitmulation/Models/Counter.swift
@@ -34,9 +34,9 @@ class Counter {
         // mmap isn't as efficient, really.
         let file = try! FileHandle.init(forReadingFrom: fileURL)
         
-        // TODO: store prevLinesDiffer: Bool, instead of having to recompare them
-        var prevPreviousLine: Person.Traits = (0, 0)
-        var previousLine: Person.Traits = (0, 0)
+        var prevLinesDiffer: Bool = false
+        var previousTraits: Person.Traits = (0, 0)
+        var currentTraits: Person.Traits = (0, 0)
 
         var shouldStop = false
         while !shouldStop {
@@ -45,38 +45,35 @@ class Counter {
                 // trying to read chunks by chunks to prevent small syscalls, and yet prevent huge memory copies.
                 // for 100 million =>    10_000 people at a time (160KB)
                 // for   1 billion => 1_000_000 people at a time  (16MB)
+                // for  10 billion => 1_000_000 people at a time  (16MB)
                 var chunk = (try? file.read(upToCount: lineLengthInBytes * 1_000_000)) ?? Data()
                 defer { chunk.removeAll(keepingCapacity: false) }
 
                 if chunk.isEmpty {
-                    if previousLine != prevPreviousLine {
+                    if prevLinesDiffer {
                         uniqueItems += 1
                     }
                     shouldStop = true
                     return
                 }
                 
+                // a lot of time is spent in subscript accesses and offset computations, maybe we could do better in C
                 chunk.withUnsafeBytes { buffer in
                     buffer.withMemoryRebound(to: UInt64.self) { lines in
                         var index = lines.startIndex
                         while lines.index(after: index) < lines.endIndex {
-                            var currentTraits = (
-                                hi: UInt64.init(bigEndian: lines[index]),
-                                lo: UInt64.init(bigEndian: lines[lines.index(after: index)])
-                            )
-                            index = lines.index(after: index)
+                            currentTraits.hi = UInt64.init(bigEndian: lines[index]) & comparingMask.hi
                             index = lines.index(after: index)
 
-                            currentTraits = (
-                                hi: currentTraits.hi & comparingMask.hi,
-                                lo: currentTraits.lo & comparingMask.lo
-                            )
+                            currentTraits.lo = UInt64.init(bigEndian: lines[index]) & comparingMask.lo
+                            index = lines.index(after: index)
 
-                            if currentTraits != previousLine && previousLine != prevPreviousLine {
+                            let differ = currentTraits.hi != previousTraits.hi || currentTraits.lo != previousTraits.lo
+                            if prevLinesDiffer && differ {
                                 uniqueItems += 1
                             }
-                            prevPreviousLine = previousLine
-                            previousLine = currentTraits
+                            previousTraits = currentTraits
+                            prevLinesDiffer = differ
                         }
                     }
                 }
